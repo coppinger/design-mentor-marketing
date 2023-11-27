7fcc657b76a7427c0aa8d91c4023c89e20be3970
Charlie Coppinger <charles.coppinger@gmail.com>
2023-11-28 11:57:03 +1300

    manually install shadnc

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,6 +4,11 @@
 
 <ul class="hidden md:flex gap-10 items-center">
 	{#each navItems as { text, path }}
-		<li><a class="font-semibold text-slate-50" href={`#${path}`}>{text}</a></li>
+		<li>
+			<a
+				class="font-semibold opacity-80 hover:opacity-100 transition-all ease-linear duration-150 delay-75"
+				href={`#${path}`}>{text}</a
+			>
+		</li>
 	{/each}
 </ul>
