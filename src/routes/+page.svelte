a0031b03b710f5ecfb91ca9f2957d9c706155e81
Charlie Coppinger <charles.coppinger@gmail.com>
2023-11-29 13:03:54 +1300

    update remaining

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -769,7 +769,7 @@
 				<div class="flex flex-col gap-8 items-center">
 					<img class="max-w-[250px]" src={premium_pricing} alt="" />
 					<p class="text-lg font-semibold text-white text-center">
-						Limited to 10 people, 7 remaining
+						Limited to 10 people, 4 remaining
 					</p>
 				</div>
 				<ul class="flex flex-col gap-6">
